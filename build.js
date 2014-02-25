bab073e175092d0245a6a6386415d31e410580ab
MakeNowJust <make.just.on@gmail.com>
2014-02-25 22:41:32 +0900

    change input patten to use eval

--- build.js
+++ build.js
@@ -35,13 +35,13 @@ program = require('commander');
 
 program
   .version('0.1.1')
-  .option('-i, --input-pattern <pattern>', 'string to replace INPUT_PATTERN of tweet.js (default: (function(){/*%s*/}).toString().match(/\/\*(.*)\*\//)[1]', String)
+  .option('-i, --input-pattern <pattern>', 'string to replace INPUT_PATTERN of tweet.js (default: (function(){/*%s*/}).toString().match(/\\/\\*(.*)\\*\\//)[1].replace(/\\{[^}]*\\}/g,eval)', String)
   .option('-o, --output <filename>', 'output filename (default: tweet.<screen_name>.js)', String)
   .option('-c, --config <filename>', 'configuration filename (default: ./config.json)', String)
   .parse(process.argv);
 
 _.defaults(program, {
-  inputPattern: '(function(){/*%s*/}).toString().match(/\\/\\*(.*)\\*\\//)[1]',
+  inputPattern: '(function(){/*%s*/}).toString().match(/\\/\\*(.*)\\*\\//)[1].replace(/\\{[^}]*\\}/g,eval)',
   output: 'tweet.<screen_name>.js',
   config: './config.json',
 });
